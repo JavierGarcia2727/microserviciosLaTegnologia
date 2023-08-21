dc5fdacb4b520b531869feb098e65923f9740d06
Javi <marvin_garcia27@hotmail.com>
2023-08-20 22:21:55 -0600

    eureka

--- usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -2,11 +2,13 @@ package com.usuario.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients //Se agrega para usar Feign
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
